325544e6a0c9e6248ebafd6df8b29e4190db9b5d
Sinclair Target <sinclairtarget@gmail.com>
2025-10-13 14:18:06 -0400

    Move backlash escape removal to inline tokenizer.

--- src/lex/BlockTokenizer.zig
+++ src/lex/BlockTokenizer.zig
@@ -273,62 +273,21 @@ fn evaluate_lexeme(
     lookahead_i: usize,
 ) !?[]const u8 {
     switch (token_type) {
-        .newline, .indent, .rule_star, .rule_underline, .rule_dash_with_whitespace => {
+        .newline, .indent, .rule_star, .rule_underline,
+        .rule_dash_with_whitespace => {
             return null;
         },
         .pound => {
-            const lexeme = try copyWithoutEscapes(
-                arena,
+            const lexeme = try arena.dupe(
+                u8,
                 std.mem.trim(u8, self.line[self.i..lookahead_i], " \t"),
             );
             return lexeme;
         },
-        .rule_equals, .rule_dash => {
+        else => {
             return try arena.dupe(u8, self.line[self.i..lookahead_i]);
         },
-        else => {
-            return try copyWithoutEscapes(arena, self.line[self.i..lookahead_i]);
-        },
     }
-}
-
-fn copyWithoutEscapes(alloc: Allocator, s: []const u8) ![]const u8 {
-    const copy = try alloc.alloc(u8, s.len);
-
-    var state: enum { normal, escape } = .normal;
-    var source_index: usize = 0;
-    var dest_index: usize = 0;
-    while (source_index < s.len) {
-        switch (state) {
-            .normal => {
-                switch (s[source_index]) {
-                    '\\' => {
-                        source_index += 1;
-                        state = .escape;
-                    },
-                    else => {
-                        copy[dest_index] = s[source_index];
-                        source_index += 1;
-                        dest_index += 1;
-                    },
-                }
-            },
-            .escape => {
-                switch (s[source_index]) {
-                    '\\' => {
-                        // literal backslash
-                        copy[dest_index] = s[source_index];
-                        source_index += 1;
-                        dest_index += 1;
-                    },
-                    else => {},
-                }
-                state = .normal;
-            },
-        }
-    }
-
-    return copy[0..dest_index];
 }
 
 test "can tokenize" {
--- src/lex/InlineTokenizer.zig
+++ src/lex/InlineTokenizer.zig
@@ -182,8 +182,52 @@ fn evaluate_lexeme(
         .newline => {
             return null;
         },
+        .text => {
+            return try copyWithoutEscapes(arena, self.in[self.i..lookahead_i]);
+        },
         else => {
             return try arena.dupe(u8, self.in[self.i..lookahead_i]);
         },
     }
+}
+
+/// Duplicates the given string, but skips all backslashes (unless they are
+/// themselves escaped).
+fn copyWithoutEscapes(alloc: Allocator, s: []const u8) ![]const u8 {
+    const copy = try alloc.alloc(u8, s.len);
+
+    var state: enum { normal, escape } = .normal;
+    var source_index: usize = 0;
+    var dest_index: usize = 0;
+    while (source_index < s.len) {
+        switch (state) {
+            .normal => {
+                switch (s[source_index]) {
+                    '\\' => {
+                        source_index += 1;
+                        state = .escape;
+                    },
+                    else => {
+                        copy[dest_index] = s[source_index];
+                        source_index += 1;
+                        dest_index += 1;
+                    },
+                }
+            },
+            .escape => {
+                switch (s[source_index]) {
+                    '\\' => {
+                        // literal backslash
+                        copy[dest_index] = s[source_index];
+                        source_index += 1;
+                        dest_index += 1;
+                    },
+                    else => {},
+                }
+                state = .normal;
+            },
+        }
+    }
+
+    return copy[0..dest_index];
 }
