58fef3bc7849366baf877c0cdaad6063ee1a1c6b
Sinclair Target <sinclairtarget@gmail.com>
2025-12-29 19:01:37 -0500

    Lower loop safety bound.

--- src/util/safety.zig
+++ src/util/safety.zig
@@ -1,3 +1,3 @@
 // It's easier to debug a panic than a hang
-pub const loop_bound: u32 = 16_777_215;
+pub const loop_bound: u32 = 100_000; // Low - make higher for production builds?
 pub const loop_bound_panic_msg = "HANG: loop safety bound exceeded";
