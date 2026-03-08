1845c7f539c2057d5cb7131a1c6bb7557c8dba88
Sinclair Target <sinclairtarget@gmail.com>
2026-03-08 10:15:11 -0400

    C header file doc updates.

--- include/atrus.h
+++ include/atrus.h
@@ -11,7 +11,8 @@ typedef enum {
 } atrus_parse_error_t;
 
 /*
- * Given a string containing MyST markdown, parses it into a MyST AST.
+ * Given a null-terminated string containing MyST markdown, parses it into a
+ * MyST AST. Returns 0 on success or a negative number on error.
  *
  * The caller is responsible for freeing the AST using atrus_free().
  */
@@ -23,8 +24,8 @@ atrus_parse_error_t atrus_parse(char* in, atrus_ast_node** out);
 void atrus_free(atrus_ast_node* root);
 
 /*
- * Given a MyST AST, renders the tree as HTML into a string. Returns the length
- * of the string or -1 on error.
+ * Given a MyST AST, renders the tree as HTML into a null-terminated string.
+ * Returns the length of the string or a negative number on error.
  *
  * The caller is responsible for freeing the string.
  */
@@ -36,13 +37,16 @@ typedef enum : unsigned int {
     ATRUS_JSON_INDENT_4 = 2,
 } atrus_json_option_whitespace_t;
 
+/*
+ * Options for JSON rendering.
+ */
 struct atrus_json_options {
     atrus_json_option_whitespace_t whitespace;
 };
 
 /*
- * Given a MyST AST, renders the tree as JSON into a string. Returns the length
- * of the string or -1 on error.
+ * Given a MyST AST, renders the tree as JSON into a null-terminated string.
+ * Returns the length of the string or a negative number on error.
  *
  * The caller is responsible for freeing the string.
  */
@@ -58,9 +62,10 @@ typedef enum {
 } atrus_load_error_t;
 
 /*
- * Given a JSON string, attempts to parse the JSON string into a MyST AST.
+ * Given a null-terminated JSON string, attempts to parse the JSON string into
+ * a MyST AST. Returns 0 on success or a negative number on error.
  * 
- * The caller is responsible for freeing the AST using atrus_free().
+ * The caller is responsible for freeing the returned AST using atrus_free().
  */
 atrus_load_error_t atrus_load_json(char* in, atrus_ast_node** out);
 
