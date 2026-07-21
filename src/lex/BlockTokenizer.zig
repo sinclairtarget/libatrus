ad61ce2780e7cd2593c3efd6525e6f76caa0a54c
Sinclair Target <sinclairtarget@gmail.com>
2026-07-21 18:59:58 -0400

    Properly tokenize block parens.

--- src/lex/BlockTokenizer.zig
+++ src/lex/BlockTokenizer.zig
@@ -428,6 +428,8 @@ fn matchText(self: Self, scratch: Allocator) !TokenizeResult {
                 '>',
                 '[',
                 ']',
+                '(',
+                ')',
                 '{',
                 '}',
                 ':',
