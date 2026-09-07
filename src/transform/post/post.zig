ab7783d15ec6600d9211d4d33d725198c8241ef6
Sinclair Target <sinclairtarget@gmail.com>
2026-09-07 15:06:49 -0400

    Split post transforms.

--- src/transform/post/post.zig
+++ src/transform/post/post.zig
@@ -6,80 +6,22 @@ const Allocator = std.mem.Allocator;
 
 const ast = @import("../../ast.zig");
 
-const State = struct {
-    container_number: u32 = 1,
-};
-
 /// Apply all "post" stage transformations.
 pub fn transform(
     alloc: Allocator,
     scratch: Allocator,
     original_node: *ast.Node,
 ) !*ast.Node {
-    var state: State = .{};
-    return try transformInternal(alloc, scratch, original_node, &state);
-}
+    var node = original_node;
 
-fn transformInternal(
-    alloc: Allocator,
-    scratch: Allocator,
-    original_node: *ast.Node,
-    state: *State,
-) !*ast.Node {
-    switch (original_node.allowedChildren()) {
-        .yes => |branch_node| switch (branch_node) {
-            .root => |n| {
-                for (0..n.children.len) |i| {
-                    n.children[i] = try transformInternal(
-                        alloc,
-                        scratch,
-                        n.children[i],
-                        state,
-                    );
-                }
-                return try transformRoot(alloc, scratch, original_node);
-            },
-            .container => |n| { // TODO: Do this in separate transformation.
-                // Enumeration for containers
-                // Want to do this in pre-order
-                if (n.enumerated) {
-                    n.enumerator = try std.fmt.allocPrintSentinel(
-                        alloc,
-                        "{d}",
-                        .{state.container_number},
-                        0,
-                    );
-                    state.container_number += 1;
-                }
+    node = try groupByBlock(alloc, scratch, node);
+    node = try enumerateContainers(alloc, scratch, node);
 
-                for (0..n.children.len) |i| {
-                    n.children[i] = try transformInternal(
-                        alloc,
-                        scratch,
-                        n.children[i],
-                        state,
-                    );
-                }
-                return original_node;
-            },
-            inline else => |n| {
-                for (0..n.children.len) |i| {
-                    n.children[i] = try transformInternal(
-                        alloc,
-                        scratch,
-                        n.children[i],
-                        state,
-                    );
-                }
-                return original_node;
-            },
-        },
-        .no => return original_node,
-    }
+    return node;
 }
 
 /// Groups all children of the root under a single block node.
-fn transformRoot(
+fn groupByBlock(
     alloc: Allocator,
     scratch: Allocator,
     node: *ast.Node,
@@ -99,4 +41,149 @@ fn transformRoot(
     root_children[0] = block;
     node.root.children = root_children;
     return node;
+}
+
+
+fn enumerateContainers(
+    alloc: Allocator,
+    scratch: Allocator,
+    original_node: *ast.Node,
+) !*ast.Node {
+    var counter: u32 = 1;
+    return try enumerateContainersInner(
+        alloc,
+        scratch,
+        original_node,
+        &counter,
+    );
+}
+
+fn enumerateContainersInner(
+    alloc: Allocator,
+    scratch: Allocator,
+    original_node: *ast.Node,
+    counter: *u32,
+) !*ast.Node {
+    switch (original_node.allowedChildren()) {
+        .yes => |branch_node| switch (branch_node) {
+            .container => |n| {
+                // Enumeration for containers
+                // Want to do this in pre-order
+                if (n.enumerated) {
+                    n.enumerator = try std.fmt.allocPrintSentinel(
+                        alloc,
+                        "{d}",
+                        .{counter.*},
+                        0,
+                    );
+                    counter.* += 1;
+                }
+
+                for (0..n.children.len) |i| {
+                    n.children[i] = try enumerateContainersInner(
+                        alloc,
+                        scratch,
+                        n.children[i],
+                        counter,
+                    );
+                }
+                return original_node;
+            },
+            inline else => |n| {
+                for (0..n.children.len) |i| {
+                    n.children[i] = try enumerateContainersInner(
+                        alloc,
+                        scratch,
+                        n.children[i],
+                        counter,
+                    );
+                }
+                return original_node;
+            },
+        },
+        .no => return original_node,
+    }
+}
+
+// ----------------------------------------------------------------------------
+// Unit Tests
+// ----------------------------------------------------------------------------
+const testing = std.testing;
+const util = @import("../../util/util.zig");
+
+test "enumerate figures" {
+    var arena = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena.deinit();
+    var alloc = arena.allocator();
+
+    const container_node_1 = try alloc.create(ast.Node);
+    container_node_1.* = .{
+        .container = .{
+            .kind = try alloc.dupeZ(u8, "figure"),
+            .enumerated = true,
+            .children = &.{},
+        },
+    };
+
+    const container_node_2 = try alloc.create(ast.Node);
+    container_node_2.* = .{
+        .container = .{
+            .kind = try alloc.dupeZ(u8, "figure"),
+            .enumerated = false,
+            .children = &.{},
+        },
+    };
+
+    const container_node_3 = try alloc.create(ast.Node);
+    container_node_3.* = .{
+        .container = .{
+            .kind = try alloc.dupeZ(u8, "figure"),
+            .enumerated = true,
+            .children = &.{},
+        },
+    };
+
+    const root_node: *ast.Node = blk: {
+        const node = try alloc.create(ast.Node);
+        const children = try alloc.dupe(*ast.Node, &[_]*ast.Node{
+            container_node_1,
+            container_node_2,
+            container_node_3,
+        });
+        node.* = .{
+            .root = .{ .children = children },
+        };
+        break :blk node;
+    };
+
+    const post_node = try transform(
+        alloc,
+        alloc,
+        root_node,
+    );
+
+    try testing.expectEqual(.root, @as(ast.NodeType, post_node.*));
+    try testing.expectEqual(1, post_node.root.children.len);
+
+    const block_node = post_node.root.children[0];
+    try testing.expectEqual(.block, @as(ast.NodeType, block_node.*));
+    try testing.expectEqual(3, block_node.block.children.len);
+
+    const first_node = block_node.block.children[0];
+    try testing.expectEqual(.container, @as(ast.NodeType, first_node.*));
+    const first_enumerator = try util.testing.expectNonNull(
+        first_node.container.enumerator,
+    );
+    try testing.expectEqualStrings("1", first_enumerator);
+
+    const second_node = block_node.block.children[1];
+    try testing.expectEqual(.container, @as(ast.NodeType, second_node.*));
+    try testing.expectEqual(null, second_node.container.enumerator);
+
+    const third_node = block_node.block.children[2];
+    try testing.expectEqual(.container, @as(ast.NodeType, third_node.*));
+    const third_enumerator = try util.testing.expectNonNull(
+        third_node.container.enumerator,
+    );
+    try testing.expectEqualStrings("2", third_enumerator);
 }
