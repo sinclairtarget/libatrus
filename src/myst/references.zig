55b6083c645f3028663805316a0b545dc67173f9
Sinclair Target <sinclairtarget@gmail.com>
2026-09-24 19:26:41 -0400

    Trim whitespace during identifier normalization.

--- src/myst/references.zig
+++ src/myst/references.zig
@@ -2,7 +2,9 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 pub fn normalizeIdentifier(alloc: Allocator, s: []const u8) ![]const u8 {
+    const trimmed = std.mem.trim(u8, s, " \t\n");
+
     // TODO: Value must be normalized such that whitespace is collapsed to a
-    // single space, initial/final space is trimmed, and case is folded
-    return try std.ascii.allocLowerString(alloc, s);
+    // single space and case is folded
+    return try std.ascii.allocLowerString(alloc, trimmed);
 }
